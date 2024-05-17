ca933b0a40eefb01fd702af9de7a51ffc13ca31d
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-05-17 21:27:42 +0530

    simple todo list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  let [todolist, setTodolist] = useState([]);
+  let saveToDoList = (event) => {
+    let toname = event.target.toname.value;
+    if (!todolist.includes(toname)) {
+      let finalTodolist = [...todolist, toname];
+      setTodolist(finalTodolist);
+    } else {
+      alert('TOdo name Already exists')
+    }
+    // alert(toname);
+    event.preventDefault();
+  }
+
+  let list = todolist.map((value, index) => {
+    return (
+      <ToDoListItems value={value} key={index} indexNumebr={index} todolist={todolist} setTodolist={setTodolist} />
+    )
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ToDo List</h1>
+      <form onSubmit={saveToDoList}>
+        <input type="text" name='toname' /><button>Save</button>
+      </form>
+
+      <div className='outerDiv'>
+        <ul>
+          {list}
+        </ul>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+function ToDoListItems({ value, indexNumebr, todolist, setTodolist }) {
+
+
+  let [status, setStatus] = useState(false);
+
+  let deleteRow = () => {
+    let finalData = todolist.filter((v, i) => i !== indexNumebr)
+    setTodolist(finalData);
+  }
+
+  let checkStatus = () => {
+    setStatus(!status)
+  }
+
+  return (
+    <li className={status ? 'completetodo' : ''} onClick={checkStatus}>{indexNumebr + 1} {value} <span onClick={deleteRow}>&times;</span></li>
+  )
+}
